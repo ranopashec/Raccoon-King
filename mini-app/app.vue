9ea58c162b9ecc128f01b55dd5437c1330b311ff
ranopashec <semenpetrakov@gmail.com>
2024-06-26 23:35:52 +0300

    simple clicker

---
+++ mini-app/app.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="flex flex-col items-center justify-center h-screen">
+    <h1 class="text-4xl font-bold mb-4">Raccoon King</h1>
+    <p class="text-xl mb-4">Clicks: {{ clicks }}</p>
+    <button @click="incrementClicks">
+      <img src="/raccoon.png" alt="Raccoon Image" class="w-64 h-auto" />
+    </button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ClickerGame",
+  data() {
+    return {
+      clicks: 0,
+    };
+  },
+  methods: {
+    incrementClicks() {
+      this.clicks++;
+    },
+  },
+};
+</script>
+
+<style>
+@import "tailwindcss/base";
+@import "tailwindcss/components";
+@import "tailwindcss/utilities";
+</style>
