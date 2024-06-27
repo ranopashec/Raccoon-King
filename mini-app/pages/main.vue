b5e75f258fe996e362cfc37cdb6a006597ab13ad
ranopashec <semenpetrakov@gmail.com>
2024-06-27 16:38:41 +0300

    change main

--- mini-app/pages/main.vue
+++ mini-app/pages/main.vue
@@ -1,51 +1,32 @@
 <template>
-  <div
-    class="font-inter flex flex-col items-center justify-center w-screen h-screen app-container touch-pan-x touch-pan-y mx-auto max-w-screen-md"
+  <main
+    class="bg-gradient-to-tr font-sans min-h-screen py-4 px-4 from-yellow-950 to-purple-950"
   >
-    <div class="grid grid-cols-2 gap-4 mb-5 w-full px-4">
-      <div
-        class="flex flex-col items-center justify-center p-2 bg-green-400 rounded-lg"
-      >
-        <span class="text-xl font-medium">Nickname</span>
-        <span>progress lvl</span>
+    <MainHeader />
+    <div
+      class="flex flex-col items-center justify-center touch-pan-x gap-8 touch-pan-y mx-auto"
+    >
+      <div class="flex flex-row w-full justify-center items-center gap-2 mt-8">
+        <img src="/coin.png" alt="Coin Image" class="w-8 h-8" />
+        <p class="text-3xl text-white font-semibold">{{ raccoons }} RCN</p>
       </div>
 
-      <div
-        class="flex flex-col items-center justify-center p-2 w-full rounded-lg bg-purple-600"
+      <button
+        @click="tap"
+        class="w-80 h-80 bg-black rounded-3xl flex flex-row items-center justify-center"
       >
-        <div>RCN/s</div>
-        <div>0</div>
-      </div>
+        <img src="/raccoon.png" alt="Raccoon Image" class="w-40" />
+      </button>
     </div>
-
-    <p class="text-xl gap-10 mb-10 mt-5">{{ clicks }} RCN</p>
-
-    <button @click="incrementClicks">
-      <img src="/raccoon.png" alt="Raccoon Image" class="w-64 h-auto" />
-    </button>
-  </div>
+  </main>
 </template>
 
-<script>
+<script setup>
 import { ref } from "vue";
+const raccoons = ref(0);
+const raccoons_per_sec = ref(0);
 
-export default {
-  name: "ClickerGame",
-  setup() {
-    const clicks = ref(0);
-
-    const incrementClicks = () => {
-      clicks.value++;
-    };
-
-    return {
-      clicks,
-      incrementClicks,
-    };
-  },
-};
+function tap() {
+  raccoons.value++;
+}
 </script>
-
-<style scoped>
-@import url("https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap");
-</style>
