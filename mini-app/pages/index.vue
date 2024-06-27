42eba8fcb82cbdff6e8e1255320873d260e0bfc4
ranopashec <semenpetrakov@gmail.com>
2024-06-27 16:38:12 +0300

    small changes

--- mini-app/pages/index.vue
+++ mini-app/pages/index.vue
@@ -4,11 +4,11 @@
     <div class="flex flex-col items-center justify-center touch-pan-x gap-8 touch-pan-y mx-auto">
       <div class="flex flex-row w-full justify-center items-center gap-2 mt-8">
         <img src="/coin.png" alt="Coin Image" class="w-8 h-8" />
-        <p class="text-3xl text-white font-semibold">{{ clicks }} RCN</p>
+        <p class="text-3xl text-white font-semibold">{{ raccoons }} RCN</p>
       </div>
 
       <button
-        @click="incrementClicks"
+        @click="tap"
         class="w-80 h-80 bg-black rounded-3xl flex flex-row items-center justify-center"
       >
         <img src="/raccoon.png" alt="Raccoon Image" class="w-40" />
@@ -17,23 +17,12 @@
   </main>
 </template>
 
-<script>
+<script setup>
 import { ref } from 'vue';
-import MainHeader from '@/components/MainHeader.vue';
+const raccoons = ref(0);
+const raccoons_per_sec = ref(0);
 
-export default {
-  name: 'ClickerGame',
-  setup() {
-    const clicks = ref(0);
-
-    const incrementClicks = () => {
-      clicks.value++;
-    };
-
-    return {
-      clicks,
-      incrementClicks,
-    };
-  },
-};
+function tap() {
+  raccoons.value++;
+}
 </script>
