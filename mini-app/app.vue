14b7b10fcdd07244f2b117a9d00157cd55e09c5e
ranopashec <semenpetrakov@gmail.com>
2024-06-27 10:19:23 +0300

    small changes

--- mini-app/app.vue
+++ mini-app/app.vue
@@ -1,7 +1,18 @@
 <template>
   <div class="flex flex-col items-center justify-center h-screen">
-    <h1 class="text-4xl font-bold mb-4">Raccoon King</h1>
+    <div class="grid grid-cols-2 gap-4">
+      <div class="flex flex-col items-center justify-center">
+        <div>nickname</div>
+        <div>progress lvl</div>
+      </div>
+
+      <div class="flex flex-col items-center justify-center">
+        <div>RCN/s</div>
+        <div>0</div>
+      </div>
+    </div>
     <p class="text-xl mb-4">Clicks: {{ clicks }}</p>
+
     <button @click="incrementClicks">
       <img src="/raccoon.png" alt="Raccoon Image" class="w-64 h-auto" />
     </button>
@@ -9,23 +20,28 @@
 </template>
 
 <script>
+import { ref } from "vue";
+
 export default {
   name: "ClickerGame",
-  data() {
-    return {
-      clicks: 0,
+  setup() {
+    const clicks = ref(0);
+
+    const incrementClicks = () => {
+      clicks.value++;
     };
-  },
-  methods: {
-    incrementClicks() {
-      this.clicks++;
-    },
+
+    return {
+      clicks,
+      incrementClicks,
+    };
   },
 };
 </script>
 
-<style>
-@import "tailwindcss/base";
-@import "tailwindcss/components";
-@import "tailwindcss/utilities";
+<style scoped>
+.grid {
+  max-width: 800px;
+  margin: 0 auto;
+}
 </style>
