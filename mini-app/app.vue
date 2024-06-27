fb4be8686e362afe2b74d77c6b54fe1715764e46
ranopashec <semenpetrakov@gmail.com>
2024-06-27 12:43:04 +0300

    main

--- mini-app/app.vue
+++ mini-app/app.vue
@@ -1,9 +1,10 @@
 <template>
   <div>
-    <Cards />
+    <Main />
   </div>
 </template>
 
 <script setup>
 import Cards from "./components/cards.vue";
+import Main from "./components/main.vue";
 </script>
