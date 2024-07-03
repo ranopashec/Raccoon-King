f1953d8d404e31f29c673629c373e92b3e30500a
ranopashec <semenpetrakov@gmail.com>
2024-07-03 10:56:20 +0300

    add some game cards

---
+++ mini-app/pages/games/pairs/Card.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="card">
+    {{ myNumber }}
+  </div>
+</template>
+
+<script setup>
+defineProps({
+  myNumber: {
+    type: Number,
+    required: true,
+  },
+});
+</script>
+
+<style>
+.card {
+  border: 5px solid #ccc;
+}
+</style>
---
+++ mini-app/pages/games/pairs/index.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <main class="bg-black text-white text-center mt-12">
+      <h1 class="text-5xl">Find a Pair</h1>
+      <section class="game-board">
+        <Card v-for="(card, index) in cardList" :key="`card-${index}`" :myNumber="card" />
+      </section>
+    </main>
+  </div>
+</template>
+
+<script setup>
+import Card from './Card.vue';
+
+const cardList = [];
+for (let i = 0; i < 16; i++) {
+  cardList.push(i);
+}
+</script>
+
+<style>
+.game-board {
+  display: grid;
+  grid-template-columns: 100px 100px 100px 100px;
+  grid-template-rows: 100px 100px 100px 100px;
+  grid-column-gap: 30px;
+  grid-row-gap: 30px;
+  justify-content: center;
+}
+</style>
