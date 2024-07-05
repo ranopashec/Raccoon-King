07b44b79997420c54bb1c1593b706428c6f92f28
Moriarte228 <t.vyshnikova@gmail.com>
2024-07-05 10:36:01 +0300

    PlusPerSEc

--- mini-app/pages/index.vue
+++ mini-app/pages/index.vue
@@ -1,8 +1,7 @@
 <template>
   <div class="bg-black">
     <main
-      class="font-sans min-h-screen touch-pan-x touch-pan-y py-4 px-4 bg-gradient-to-r from-0% via-20 via-50% to-100% from-purple-950/40 via-purple-950/20 to-purple-950/40"
-    >
+      :class="classStyle">
       <MainHeader />
       <div class="flex flex-col items-center justify-center gap-8 mx-auto">
         <button
@@ -23,6 +22,7 @@ import { ref, inject, computed, onMounted, onBeforeUnmount } from 'vue';
 const raccoonsPerSec = inject('raccoonsPerSec');
 const raccoons = inject('raccoons');
 const raccoonsPerClick = inject('raccoonsPerClick');
+const classStyle = inject('classStyle');
 
 const timer = ref();
 
